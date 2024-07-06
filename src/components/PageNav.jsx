39fc5046983d3f1a83acc335f4dd7e729121caa0
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2024-07-06 21:10:41 +0300

    finished worldwise project

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import styles from "./PageNav.module.css"
-import Logo from "./Logo"
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import styles from "./PageNav.module.css";
 
-export default function PageNav() {
+function PageNav() {
   return (
     <nav className={styles.nav}>
-        <Logo />
-        <ul>
-            <li>
-                <NavLink to="/pricing">Pricing</NavLink>
-            </li>
-            <li>
-                <NavLink to="/product">Product</NavLink>
-            </li>
-            <li>
-                <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
-            </li>
-        </ul>
+      <Logo />
+
+      <ul>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
+
+export default PageNav;
